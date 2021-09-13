dba729bb177a788b677de2d7cba341e634080310
fromjaytoz <76098411+fromjaytoz@users.noreply.github.com>
2021-09-13 15:47:00 +0800

    added state to char creation form

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,43 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [str, setStr] = useState(0);
+  const [agi, setAgi] = useState(0);
+  const [int, setInt] = useState(0);
+
   return (
     <div>
       <form>
         <label>Character Name</label>
-        <input></input>
+        <input
+          value={name || ""}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        ></input>
         <label>Strength</label>
-        <input></input>
+        <input
+          value={str || ""}
+          onChange={(e) => {
+            setStr(e.target.value);
+          }}
+        ></input>
         <label>Agility</label>
-        <input></input>
+        <input
+          value={agi || ""}
+          onChange={(e) => {
+            setAgi(e.target.value);
+          }}
+        ></input>
         <label>Intelligence</label>
-        <input></input>
+        <input
+          value={int || ""}
+          onChange={(e) => {
+            setInt(e.target.value);
+            console.log(int);
+          }}
+        ></input>
       </form>
     </div>
   );
