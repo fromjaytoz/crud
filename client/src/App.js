5e4b14d187c20f6c2ac17649d79792eef9199669
fromjaytoz <76098411+fromjaytoz@users.noreply.github.com>
2021-09-13 16:26:47 +0800

    char creation form adds character model to db

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
 import { useState } from "react";
-
+import axios from "axios";
 function App() {
   const [name, setName] = useState("");
   const [str, setStr] = useState(0);
   const [agi, setAgi] = useState(0);
   const [int, setInt] = useState(0);
+  const [charClass, setCharClass] = useState("");
+
+  const sendStatsToServer = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:3001/", {
+        name,
+        str,
+        agi,
+        int,
+        charClass,
+      });
+      await console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div>
-      <form>
+      <form onSubmit={(e) => sendStatsToServer(e)}>
         <label>Character Name</label>
         <input
           value={name || ""}
@@ -38,6 +55,14 @@ function App() {
             console.log(int);
           }}
         ></input>
+        <label>Class</label>
+        <input
+          value={charClass || ""}
+          onChange={(e) => {
+            setCharClass(e.target.value);
+          }}
+        ></input>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,28 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
 const PlayerModel = require("./models/Player");
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(
   "mongodb+srv://OC:12345@crud.wmgom.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   { useNewUrlParser: true }
 );
 
-app.get("/", (req, res) => {
+app.post("/", async (req, res) => {
+  const { name, str, agi, int, charClass } = req.body;
   const Player = new PlayerModel({
-    name: "Lol you suck Trisha",
-    strength: 9,
-    agility: 10,
-    intelligence: 50,
+    name: name,
+    strength: str,
+    agility: agi,
+    intelligence: int,
+    class: charClass,
   });
   try {
-    Player.save();
+    await Player.save();
     res.send(Player);
   } catch (err) {
     console.log(err);
+    res.send(err);
   }
 });
 
