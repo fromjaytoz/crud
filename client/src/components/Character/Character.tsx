446366bd8e0aa5f8682ea3d95e2fb45c895d5afb
fromjaytoz <76098411+fromjaytoz@users.noreply.github.com>
2021-09-17 16:04:24 +0800

    CONTRIBUTOR: fromjaytoz ADDED: clickable character edit with onBlur value change NEXT: add onBlur event when 'Enter' is pressed

--- client/src/components/Character/Character.tsx
+++ client/src/components/Character/Character.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useReducer, useEffect } from "react";
 import CharacterInterface from "../../interfaces/CharacterInterface";
 import axios from "axios";
+
+interface AttributeState {
+  value: number | string;
+  render: any;
+  clicked: boolean;
+}
+
+interface ReducerAction {
+  type: string;
+  payload: number;
+}
 
 const Character: React.FC<CharacterInterface> = ({
   agility,
@@ -10,6 +21,47 @@ const Character: React.FC<CharacterInterface> = ({
   strength,
   _id,
 }) => {
+  const initialState = (attribute: number | string) => {
+    return {
+      value: attribute,
+      render: <span>{attribute}</span>,
+      clicked: false,
+    };
+  };
+  const reducer = (state: any, action: any) => {
+    switch (action.type) {
+      case "unclicked":
+        console.log(state);
+        return {
+          ...state,
+          render: (
+            <input
+              onChange={(e) => {
+                const valueIsString = typeof state.value === "string";
+                const inputValue = e.target.value;
+                state.value = valueIsString
+                  ? inputValue
+                  : Number(
+                      isNaN(Number(inputValue)) ? state.value : inputValue
+                    );
+                console.log(state);
+              }}
+              onBlur={async (e) => {
+                console.log(state.value);
+                dispatch({ type: "clicked", payload: state.value });
+              }}
+              onKeyUp={(e) => {
+                e.keyCode === 13 && e.target.addEventListener("blur", () => {});
+              }}
+            ></input>
+          ),
+        };
+      case "clicked":
+        return { ...state, render: <span>{action.payload}</span> };
+    }
+  };
+  const [strengthState, dispatch] = useReducer(reducer, initialState(strength));
+
   const deleteChar = (charId: string) => {
     try {
       axios.delete("http://localhost:3001/deleteChar", {
@@ -21,10 +73,17 @@ const Character: React.FC<CharacterInterface> = ({
       console.log(err);
     }
   };
+
   return (
     <div>
       <h1>{name}</h1>
-      <p>Strength:{strength}</p>
+      <p
+        onClick={() => {
+          dispatch({ type: "unclicked" });
+        }}
+      >
+        Strength:{strengthState?.render}
+      </p>
       <p>Agility:{agility}</p>
       <p>Intelligence:{intelligence}</p>
       <p>Class:{charClass}</p>
