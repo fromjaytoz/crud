720e098025cc700ca1f9d529a28ceab9b7617951
TrishaZapanta <trisha05zapanta@gmail.com>
2021-09-26 22:20:22 +0800

    merging with main branch

--- client/src/components/Character/Character.test.tsx
+++ client/src/components/Character/Character.test.tsx
@@ -1,20 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import Character from "../Character/Character";
 
-// jest.mock("http://localhost:3001/all");
-describe("test", () => {
+describe("Tests Character component's functionality", () => {
+  const testCharacter = (
+    <Character
+      name={"Test"}
+      strength={0}
+      agility={0}
+      intelligence={0}
+      charClass={"Test"}
+      _id="i-am-a-test"
+    />
+  );
   test("A given character component renders properly", async () => {
-    await render(
-      <Character
-        name={"Test"}
-        strength={0}
-        agility={0}
-        intelligence={0}
-        charClass={"Test"}
-        _id="1"
-      />
-    );
-    const character = await screen.findByTestId("character-1");
+    await render(testCharacter);
+    const character = await screen.findByTestId("character-i-am-a-test");
     expect(character).toBeInTheDocument();
   });
 });
